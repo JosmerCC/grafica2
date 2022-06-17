476fa3fc95ecfef63e738d1d8d934c2a76524539
JosmerCC <106988741+JosmerCC@users.noreply.github.com>
2022-06-16 22:58:48 -0500

    Update g_distritos.py

--- g_distritos.py
+++ g_distritos.py
@@ -4,8 +4,10 @@ import numpy as np
 import urllib.request
 
 @st.experimental_memo
-def download_data:
-  url="http://server01.labs.org.pe:2005/datos_horarios_contaminacion_lima.csv"
-  filename="datos_horarios.csv"
-  urllib.request.urlretrieve(url,filename)
-download_data()
+def download_data():
+   url="http://server01.labs.org.pe:2005/datos_horarios_contaminacion_lima.csv"
+   filename="datos_horarios.csv"
+   urllib.request.urlretrieve(url,filename)
+   df=pd.read_csv('datos_horarios.csv')
+   return df
+st.dataframe(download_data())
